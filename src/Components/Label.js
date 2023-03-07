47087eaff539cef70171f4129f76b7bb093be5c1
ParaJay <calumjhunter@gmail.com>
2023-03-07 21:41:38 +0000

    labels can now be expanded

--- src/Components/Label.js
+++ src/Components/Label.js
@@ -6,26 +6,37 @@ const Label = (props) => {
     if(props.isChecked) labelClassName += " checked";
     if(props.isHidden) labelClassName += " hidden";
 
-    return (
-        <div className={hidden ? "hidden" : "todo-wrapper"}>
-            <h3 className={labelClassName}>{props.title}</h3>
-            <p className={labelClassName}>{props.text}</p>
-
-            <div className="wrapper">
-                <div>
-                    <button onClick={props.onCheck}>Check</button>
+    if(!props.isExpanded) {
+        return (
+            <div className={hidden ? "hidden" : "todo-wrapper"} onClick={props.onClick}>
+                <h3 className={labelClassName}>{props.title}</h3>
+            </div>
+        )
+    } else {
+        return (
+            <div className={hidden ? "hidden" : "todo-wrapper"}>
+                <div className="sub-wrapper" onClick={props.onClick}>
+                    <h3 className={labelClassName}>{props.title}</h3>
+                    <p className={labelClassName}>{props.text}</p>
                 </div>
-
-                <div className="centered-item">
-                <button onClick={props.onHide}>Hide</button>
-                </div>
-
-                <div className="right-item">
-                    <button onClick={props.onRemove}>Delete</button>
+                
+    
+                <div className="wrapper">
+                    <div>
+                        <button onClick={props.onCheck}>Check</button>
+                    </div>
+    
+                    <div className="centered-item">
+                        <button onClick={props.onHide}>Hide</button>
+                    </div>
+    
+                    <div className="right-item">
+                        <button onClick={props.onRemove}>Delete</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 export default Label;
