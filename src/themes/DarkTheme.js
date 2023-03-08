faca59ee3809365c68205bae604eef5550f3f40a
ParaJay <calumjhunter@gmail.com>
2023-03-08 16:12:07 +0000

    added themes

---
+++ src/themes/DarkTheme.js
@@ -0,0 +1,12 @@
+const DarkTheme = () => {
+    return (
+        `
+        .themed {
+            background-color: #333;
+            color: #fff;
+        }
+        `
+    )
+}
+
+export default DarkTheme;
---
+++ src/themes/LightTheme.js
@@ -0,0 +1,12 @@
+const LightTheme = () => {
+    return (
+        `
+        .themed {
+            background-color: #fff;
+            color: #333;
+        }
+        `
+    )
+}
+
+export default LightTheme;
