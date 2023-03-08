9161846d010dfc08e9400bb5651d27876a069a5e
ParaJay <calumjhunter@gmail.com>
2023-03-08 11:50:27 +0000

    added search bar, added Display(), added location for display order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
+import Input from "./Components/Input";
 import NoteDialog from "./Components/NoteDialog";
 import Notes from "./Components/Notes";
 
@@ -10,11 +11,14 @@ const App = () => {
     const [notes, setNotes] = useState([]);
     const [dialog, setDialogue] = useState(false);
     const [block, setBlock] = useState(false);
+    const [search, setSearch] = useState("");
+    const [location, setLocation] = useState("above");
 
     const colours = {};
 
     const onTitleChange = (e) => { setTitle(e.target.value); }
     const onTextChange = (e) => { setText(e.target.value); }
+    const onSearchChange = (e) => setSearch(e.target.value)
 
     const changeNote = (index, key, value, set=true) => {
         let tds = [...notes];
@@ -77,6 +81,8 @@ const App = () => {
 
     const expandNote = (index) => {
         changeNote(index, "expanded", "invert");
+
+        if(notes[index].expanded) document.getElementById(notes[index].title).scrollIntoView(false);
     }
 
     const hideNote = (index) => {
@@ -126,21 +132,65 @@ const App = () => {
         setBlock(!block);
     }
 
+    const toggleLocation = () => {
+        setLocation(location === "above" ? "below" : "above");
+        setTimeout(() => {
+            if(location === "above") {
+                window.scrollTo(0, 0);
+            } else {
+                window.scrollTo(0, window.screen.height);
+            }
+        });
+    }
+
     const ND = <NoteDialog visible={dialog} closeCallback={
         () => setDialogue(false)
     } onOverwrite={() => {overwriteNote(); addNote();}} onMerge={mergeNote}/>;
 
-    return (
-        <> 
-            {ND}
-            <Notes block={block} notes={notes} onCheck={checkNote} onRemove={removeNote} onHide={hideNote} onClick={expandNote} onColour={colourNote} colours={colours}></Notes>
-            <br/><br/><br/>
-            <Form onSubmit={addNote} onTextChange={onTextChange} onTitleChange={onTitleChange}></Form>
-            <br/><br/>
-            <button onClick={showAll}>Show All</button>
-            <button onClick={() => toggleBlock()}>View as {block ? "Rows" : "Blocks"}</button>
-        </>
-      );
+    const Display = () => {
+        let first;
+        let second;
+
+        let n = <Notes filter={search} block={block} notes={notes} onCheck={checkNote} onRemove={removeNote} onHide={hideNote} onClick={expandNote} onColour={colourNote} colours={colours}></Notes>
+        let w = (
+            <div className="wrapper">
+                <div>
+                    <Form onSubmit={addNote} onTextChange={onTextChange} onTitleChange={onTitleChange}></Form>
+                </div>
+
+                <div className="column-wrapper right-item note-wrapper">
+                    <div className="right-item">
+                        <button onClick={showAll}>Show All</button>
+                        <button onClick={() => toggleBlock()}>View as {block ? "Rows" : "Blocks"}</button>
+                    </div>
+
+                    <div className="right-item">
+                        <Input onChange={onSearchChange} text="Search"></Input>
+                    </div>
+
+                    <button id="change-display-loc" onClick={toggleLocation}>{"display: " + (location === "above" ? "below" : "above")}</button>
+                </div>
+            </div>
+        )
+
+        if(location === "above") {
+            first = n;
+            second = w;
+        } else {
+            first = w;
+            second = n;
+        }
+
+        return (
+            <>
+                {ND}
+                {first}
+                {second}
+            </>
+        )
+    }
+
+    return Display();
 }
 
 export default App;
