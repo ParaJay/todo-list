9b870e54f89686b9fd5aebfb5c0072966767ad59
ParaJay <calumjhunter@gmail.com>
2023-03-08 00:33:28 +0000

    changed function names. changed props.open to props.visible

--- src/Components/NoteDialog.js
+++ src/Components/NoteDialog.js
@@ -1,59 +1,59 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 
 const customStyles = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
     },
-  };
+};
 
-  Modal.setAppElement('#root');
+Modal.setAppElement('#root');
 
 function NoteDialog(props) {
     let subtitle;
-    const [modalIsOpen, setIsOpen] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
     const closeCallback = props.closeCallback;
-  
-    function afterOpenModal() {
-      subtitle.style.color = '#f00';
+
+    function afterVisible() {
+        subtitle.style.color = '#f00';
     }
-  
-    function closeModal(callback) {
-      setIsOpen(false);
-      let func = () => {};
-      if(typeof callback === typeof func) callback();
-      closeCallback();
+
+    function dispose(callback) {
+        setIsVisible(false);
+        let func = () => {};
+        if (typeof callback === typeof func) callback();
+        closeCallback();
     }
 
     const onSubmit = (callback) => {
-        closeModal(callback);
+        dispose(callback);
     }
 
-    if(props.open && !modalIsOpen) {
-        setIsOpen(true);
+    if (props.visible && !isVisible) {
+        setIsVisible(true);
     }
-  
+
     return (
         <Modal
-        isOpen={modalIsOpen || props.open}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={() => closeModal(props.closeCallback)}
+        isOpen={isVisible}
+        onAfterOpen={afterVisible}
+        onRequestClose={() => dispose(props.closeCallback)}
         style={customStyles}
         contentLabel="Example Modal"
-      >
+        >
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>This Note already exists, what do you want to do?</h2>
         <div className="wrapper">
             <button className="left-item" onClick={() => onSubmit(props.onOverwrite)}>Overwrite</button>
             <button onClick={() => onSubmit(props.onMerge)}>Merge</button>
             <button className="right-item" onClick={onSubmit}>Cancel</button>
         </div>
-      </Modal>
-    );
-  }
+        </Modal>
+        );
+    }
 
   export default NoteDialog;
