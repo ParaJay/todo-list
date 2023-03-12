e457eacae5be3050ad00e8e4685df8c9d58be45e
ParaJay <calumjhunter@gmail.com>
2023-03-12 18:46:22 +0000

    now supports images

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,4 +1,14 @@
 const Button = (props) => {
+    if(props.image) {
+        return (
+            <button onClick={props.onClick} className={props.className} id={props.id}>
+                <img src={props.image} className={props.imageClassName + " icon"} onClick={(e) => {
+                    e.stopPropagation();
+                    if(props.onClick) props.onClick();
+                }}/>
+            </button>
+        )
+    }
     return (
         <button id={props.id} className={props.className + " themed"} onClick={props.onClick}>{props.text}</button>
     )
