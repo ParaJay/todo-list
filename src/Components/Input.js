fe57083bb314f4a23e4148db7fa8f5399256d00e
ParaJay <calumjhunter@gmail.com>
2023-03-12 18:46:36 +0000

    removed cols/rows, added defaultValue

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -4,20 +4,21 @@ const Input = (props) => {
     const placeholder = `Enter ${props.text}...`
     const onChange = props.onChange;
     const className = "note-input themed " + props.className;
+    const defaultValue = props.defaultValue;
 
     if(props.isTextArea) {
         return (
-            <textarea className={className}
+            <textarea className={className + " textarea"}
                 placeholder={placeholder}
                 required={true} 
-                cols={56} 
-                rows={28} 
                 resizable="false" 
-                onChange={onChange}>
+                onChange={onChange}
+                defaultValue={defaultValue}
+                >
             </textarea>
         );
     } else {
-        return (<input className={className} required={true} onChange={onChange} placeholder={placeholder}></input>);
+        return (<input className={className} required={true} onChange={onChange} placeholder={placeholder} defaultValue={defaultValue} value={props.value}></input>);
     }
 }
 
