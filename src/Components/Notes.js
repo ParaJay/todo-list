46633bd55e20fe98faa5a910367da605d4cff911
ParaJay <calumjhunter@gmail.com>
2023-03-12 18:47:47 +0000

    removed under-border from overflow

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -66,7 +66,7 @@ const Notes = (props) => {
 
     if(curr.length > 0) sepWrapped.push(<div key={"c-w-x" } className="wrapper">{curr}</div>);
 
-    return <div className="overflow under-border">{unwrapped}{sepWrapped}</div>;
+    return <div className="overflow">{unwrapped}{sepWrapped}</div>;
 }
 
 export default Notes;
