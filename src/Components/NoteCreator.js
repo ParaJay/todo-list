431efe86241360db66033606420217f7fd30b968
ParaJay <calumjhunter@gmail.com>
2023-03-12 18:47:03 +0000

    now uses Panel instead of Modal

--- src/Components/NoteCreator.js
+++ src/Components/NoteCreator.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "react-modal";
+import { getSubOrDefault } from "../themes";
+import Button from "./Button";
 import Input from "./Input";
+import Panel from "./Panel";
+import Settings from "./Settings";
 
 const customStyles = {
     content: {
@@ -15,44 +19,42 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
-function NoteCreator(props) {
-    let subtitle;
-    const [isVisible, setIsVisible] = useState(false);
+const NoteCreator = (props) => {
+    customStyles.content["backgroundColor"] = getSubOrDefault(props.theme, "backgroundColor", "#fff");
 
-    function afterVisible() {
-    }
-
-    function dispose() {
-        setIsVisible(false);
-        props.onClose();
-    }
-
-    if (props.isVisible && !isVisible) {
-        setIsVisible(true);
-    }
-
-    if(props.theme === "dark") {
-        customStyles.content.backgroundColor = "#333";
+    const onSubmit = (e) => {
+        e.stopPropagation();
+        
+        props.onSubmit();
     }
 
     return (
-        <Modal
-        isOpen={isVisible}
-        onAfterOpen={afterVisible}
-        onRequestClose={() => dispose(props.closeCallback)}
-        style={customStyles}
-        contentLabel="Existing Note"
-        >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Create Note</h2>
-        <form onSubmit={props.onSubmit}>
-            <Input onChange={props.onTitleChange} text="Note Title"></Input>
-            <br/>
-            <Input isTextArea={true} onChange={props.onTextChange} text="note"></Input>
-            <br/>
-            <input className="themed" type="submit" value="Add Note"></input>
-        </form>
-        </Modal>
-        );
-    }
+        <Panel
+            header = {[
+                <Button key="back" onClick={props.onBack} className="back-btn left nav-item" text="<"></Button>,
+                props.settingsButton,
+                <Input key="input" className="invis center-nav-item nav-item" onChange={props.onTitleChange} text="Note Title"></Input>
+                ]}
+
+            body = {(
+                <div className="overflow">
+                    <form id="create-form" className="max" onSubmit={(e) => onSubmit(e)}>
+                        <Input className="top top-label nav-item" onChange={props.onTitleChange} text="Note Title" value={props.title}></Input>
+                        <Input isTextArea={true} onChange={props.onTextChange} text="note" defaultValue={props.text}></Input>
+                        <Button className={"confirm-note btm-right"} text="&#10003;"/>
+                    </form>
+                </div>
+            )}
+
+            settings = {(
+                <Settings isVisible={props.isEditingSettings} onClose={props.onSettingsClose} settings = {
+                    {
+                        "seperator1": ""
+                    }
+                } theme={props.theme}></Settings>  
+            )}
+        />
+    );
+}
 
   export default NoteCreator;
